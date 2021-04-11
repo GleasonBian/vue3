57dc637ef6ad3bd82df12842b3a1bc1ea8fac73c
Gleason <bianliuzhu@gmail.com>
2021-04-11 20:34:27 +0800

    feat: 格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/typescript/recommended',
+		'@vue/prettier',
+		'@vue/prettier/@typescript-eslint'
+	],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/camelcase': 'off',
+		indent: ['error', 'tab'],
+		semi: 2, // 是否需要分号
+		camelcase: 2, // 强制驼峰法命名
+		'prettier/prettier': 0
+	}
 };
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
+const path = require('path');
 // function resolve (dir) {
 //   return path.join(__dirname, dir)
 // }
 // mock 数据插件
-const MockjsWebpackPlugin = require('mockplugin')
+const MockjsWebpackPlugin = require('mockplugin');
 const {
 	NODE_ENV, // 环境变量
 	VUE_APP_ENV, // 环境标识
@@ -11,32 +11,39 @@ const {
 	VUE_APP_TMGI, // 业务标识
 	VUE_APP_HOST, // 项目运行 域名
 	VUE_APP_PORT, // 项目运行 端口
-	VUE_APP_URL, // 接口域名
+	VUE_APP_URL // 接口域名
 	// VUE_APP_TITLE, // 网页标题
 	// VUE_APP_STATIC_HOST, // 静态文件域名
 	// VUE_APP_DIR, // 静态文件二级文件夹
 } = process.env;
 
 // 是否为生产模式
-const IS_PROD = NODE_ENV === 'production'
+const IS_PROD = NODE_ENV === 'production';
 
 // 是否为 mock 环境
-const IS_MOCK = VUE_APP_ENV === 'mock'
+const IS_MOCK = VUE_APP_ENV === 'mock';
 
 // 是否为 准生产 环境
-const IS_OFFICIAL = Boolean(VUE_APP_OFFICIAL !== undefined && VUE_APP_OFFICIAL === 'true')
+const IS_OFFICIAL = Boolean(
+	VUE_APP_OFFICIAL !== undefined && VUE_APP_OFFICIAL === 'true'
+);
 
-console.log('-----------------------------overview-----------------------------')
+console.log(
+	'-----------------------------overview-----------------------------'
+);
 console.log('当前模式:', NODE_ENV);
 console.log('环境标识:', VUE_APP_ENV);
 console.log('接口域名:', VUE_APP_URL);
 if (IS_PROD) {
-	VUE_APP_OFFICIAL !== undefined && console.log('是准生产:', IS_OFFICIAL ? '是' : '否')
+	VUE_APP_OFFICIAL !== undefined &&
+		console.log('是准生产:', IS_OFFICIAL ? '是' : '否');
 	console.log('业务标识:', IS_OFFICIAL ? '无' : VUE_APP_TMGI);
 } else {
 	console.log('运行域名:', `${VUE_APP_HOST}:${VUE_APP_PORT}`);
 }
-console.log('-----------------------------overview-----------------------------')
+console.log(
+	'-----------------------------overview-----------------------------'
+);
 // 插件配置
 function pluginsConfig() {
 	const plugins = [];
@@ -44,7 +51,7 @@ function pluginsConfig() {
 		plugins.push(
 			new MockjsWebpackPlugin({
 				path: path.join(__dirname, './mock'),
-				port: 9007,
+				port: 9007
 			})
 		);
 	}
@@ -73,16 +80,16 @@ module.exports = {
 			alias: {
 				'@': path.resolve(__dirname, './src'),
 				'@a': path.resolve(__dirname, './src/assets'),
-				'@c': path.resolve(__dirname, './src/components'),
-			},
+				'@c': path.resolve(__dirname, './src/components')
+			}
 		},
-		plugins: pluginsConfig(),
+		plugins: pluginsConfig()
 	},
 	// 代理服务
 	devServer: {
 		overlay: {
 			warnings: false,
-			errors: true,
+			errors: true
 		},
 		compress: true,
 		open: true, // 启动时 是否 打开浏览器
@@ -97,9 +104,9 @@ module.exports = {
 				target: VUE_APP_URL, // 代理目标
 				changeOrigin: true, // 覆盖主机头来源
 				pathRewrite: {
-					['^/' + VUE_APP_ENV]: '',
-				},
-			},
-		},
-	},
+					['^/' + VUE_APP_ENV]: ''
+				}
+			}
+		}
+	}
 };
