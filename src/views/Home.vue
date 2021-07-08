25a40f4b9929b6254bac56684d2372eac016f8be
卞刘柱 <ext.bianliuzhu@jd.com>
2021-07-08 09:41:53 +0800

    feat: 增加标签,侧边栏,页头组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,22 +7,19 @@
 -->
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
+		<!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
 		<button @click="sendRequest">发送请求测试</button>
-		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+		<!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
 	</div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import { login } from '@/api/index.js';
 
 @Options({
 	methods: {},
-	components: {
-		HelloWorld
-	}
+	components: {}
 })
 export default class Home extends Vue {
 	async sendRequest() {
